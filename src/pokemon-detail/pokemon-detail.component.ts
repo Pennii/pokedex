99fb98c4b9bf0f6a9733c17bcf897e8ef035bf62
penni <matiasspennino@gmail.com>
2025-02-19 13:36:15 +0100

    componentes pokemon

---
+++ src/pokemon-detail/pokemon-detail.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-detail',
+  imports: [],
+  templateUrl: './pokemon-detail.component.html',
+  styleUrl: './pokemon-detail.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PokemonDetailComponent { 
+
+  @Input() pokemon: string = '';
+  @Input() imagen: string = '';
+  @Input() habilities: Array<string> = [];
+
+  @Output() clickName = new EventEmitter<string>();
+}
